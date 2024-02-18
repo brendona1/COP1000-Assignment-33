c2d9facb22d64d8675622f4fbbbc761163c45aa1
bandujar2 <31085528-bandujar2@users.noreply.replit.com>
2024-02-18 19:00:57 +0000

    assignment

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
-# Function:     This program determines if a date entered by the user is valid.  
-# Input:        Interactive
-# Output:       Valid date is printed or user is alerted that an invalid date was entered.
-
 validDate = True
 MIN_YEAR = 0
 MIN_MONTH = 1
 MAX_MONTH = 12
 MIN_DAY = 1
 MAX_DAY = 31
-
-year = None
-month = None
-day = None
-
-# Get the year, then the month, then the day
-# housekeeping()
-
-# Check to be sure date is valid
-
-if int(year) <= MIN_YEAR: # invalid year
-    validDate = False
-elif int(month) < MIN_MONTH or int(month) > MAX_MONTH: # invalid month
-    validDate = False
-elif int(day) < MIN_DAY or int(day) > MAX_DAY: # invalid day
-    validDate = False
-
-# Test to see if date is valid and output date and whether it is valid or not
-
-# endOfJob()
-if validDate == True:
-    # Output statement
+year = int(input("Enter the year: "))
+month = int(input("Enter the month: "))
+day =  int(input("Enter the day: "))
+if int(year) <= MIN_YEAR: 
+ validDate = False
+elif int(month) < MIN_MONTH or int(month) > MAX_MONTH: 
+ validDate = False
+elif int(day) < MIN_DAY or int(day) > MAX_DAY: 
+ validDate = False
+if validDate is True:
+    print(str(month) + "/" + str(day) + "/" + str(year) + " is a valid date")
 else:
-    # Output statement
+    print(str(month) + "/" + str(day) + "/" + str(year) + " is an invalid date")
